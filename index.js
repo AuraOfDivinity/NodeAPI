472636a83c0efd087ff036777b73e9b4e65dce55
Asel Peiris <peirisasel373@gmail.com>
2019-04-21 11:47:58 +0530

    Payload management

--- index.js
+++ index.js
@@ -27,13 +27,25 @@ const server = http.createServer((req, res) => {
     let headers = req.headers
 
     //Get the payload if there is any
+    let decoder =  new StringDecoder('utf-8')//payloads that come into the server through requests come as streams (bit by bit). When the stream mentions its at the endm the entire payload is taken and then decoded
+    let buffer = ''
+    
+    req.on('data', (data) =>{
+        buffer += decoder.write(data)
+    })// req.on's 
 
+    req.on('end',() =>{
+        buffer += decoder.end()
 
-    //Sending the response
-    res.end("Hello world!\n")
+        //Sending the response
+        res.end("Hello world!\n")
 
-    //Log the path user is asking for
-    console.log('Request received with these headers:', headers)
+        //Log the path user is asking for
+        console.log('Request received with these headers:', headers)
+        console.log('Request received with this payload:', buffer)
+    })
+
+    
 
     
 })
